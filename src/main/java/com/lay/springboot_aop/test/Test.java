94330df26eb31216a1a6edaad3b7a939ce1540c1
zhaogengsheng <1778038380@qq.com>
2018-10-23 22:48:42 +0800

    add GeneralProxy demo

--- src/main/java/com/lay/springboot_aop/test/Test.java
+++ src/main/java/com/lay/springboot_aop/test/Test.java
@@ -1,16 +1,23 @@
 package com.lay.springboot_aop.test;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 import com.lay.springboot_aop.intercept.impl.MyInterceptor;
 import com.lay.springboot_aop.proxy.ProxyBean;
 import com.lay.springboot_aop.service.HelloService;
 import com.lay.springboot_aop.service.impl.HelloServiceImpl;
+import com.sun.org.apache.bcel.internal.generic.INVOKEVIRTUAL;
 
 public class Test {
     
     public static void main(String[] args) {
-        testProxy();
+    	testProxy();
     }
+    
     private static void testProxy() {
+    	
         HelloService helloService=new HelloServiceImpl();
         //按照约定获取proxy
         HelloService proxy=(HelloService)ProxyBean.getProxyBean(helloService,new MyInterceptor());
@@ -20,6 +27,23 @@ public class Test {
         proxy.sayHello(null);
         System.out.println("\n################ invoke method sayBye with return value ##########################\n");
         String words=proxy.sayBye("goodbye");
+        System.out.println("in test main  "+words);
+    }
+    
+    
+    
+    private static void testGeneralProxy() {
+    	HelloService helloService=new HelloServiceImpl();
+    	HelloService proxy=(HelloService) Proxy.newProxyInstance(
+    			helloService.getClass().
+    			getClassLoader(), helloService.getClass().getInterfaces(),
+    			new InvocationHandler() {
+			@Override
+			public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+				return method.invoke(helloService, args);
+			}
+		});
+    	String words=proxy.sayBye("goodbye");
         System.out.println("in test main  "+words);
     }
 }
